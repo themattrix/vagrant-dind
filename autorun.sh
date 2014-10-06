2220ed21b1534ad7893c42f39beefa5dc775d698
Matthew Tardiff <mattrix@gmail.com>
2014-10-06 19:26:43 +0000

    Bash history is now preserved between sessions.

--- autorun.sh
+++ autorun.sh
@@ -3,6 +3,7 @@
 docker run -i -t --rm --privileged=true \
     -v /mnt/sda/var/lib/docker-inner:/var/lib/docker \
     -v /mnt/sda/repos:/repos \
+    -v /mnt/sda/history:/home/.bash_history \
     -v /home/docker/.ssh-inner:/home/.ssh \
     -v /vagrant:/vagrant \
     develop
